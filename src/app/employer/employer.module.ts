f9abdcc821797ca3d0e129ea79719490c658e3dc
234mati <mateusz.pietrych@gmail.com>
2023-10-08 22:08:27 +0200

    MIWM-194: Adding main modules

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {EmployerRoutingModule} from './employer-routing.module';
+import { EmployerRoutingModule } from './employer-routing.module';
 
 
 @NgModule({
