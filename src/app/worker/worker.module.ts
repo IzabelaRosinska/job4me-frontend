06e6f38bf9c749a13ee7d1f87e8c38a5166648cd
234mati <mateusz.pietrych@gmail.com>
2023-10-09 12:50:08 +0200

    MIWM-214: Adding navigation bar and setup basic navigation

--- src/app/worker/worker.module.ts
+++ src/app/worker/worker.module.ts
@@ -1,22 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { WorkerRoutingModule } from './worker-routing.module';
+import {WorkerRoutingModule} from './worker-routing.module';
 import {UtilitiesModule} from "../utilities/utilities.module";
-import { WorkerAccountComponent } from './worker-account/worker-account.component';
 
 
 @NgModule({
-  declarations: [
-    WorkerAccountComponent
-  ],
-  exports: [
-    WorkerAccountComponent
-  ],
+  declarations: [],
+  exports: [],
   imports: [
     CommonModule,
     WorkerRoutingModule,
     UtilitiesModule
   ]
 })
-export class WorkerModule { }
+export class WorkerModule {
+}
