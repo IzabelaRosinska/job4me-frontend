da21fd5a60b92f988dece4af6ef69909440982e6
234mati <mateusz.pietrych@gmail.com>
2023-10-30 00:34:49 +0100

    MIWM-222: Update types

--- src/app/employer/job-offer/job-offer.component.ts
+++ src/app/employer/job-offer/job-offer.component.ts
@@ -10,18 +10,18 @@ export class JobOfferComponent {
 
   jobOfferData: JobOffer = {
     id: "8436248",
-    name: "Junior Java Developer",
+    offerName: "Junior Java Developer",
     company: {  id: "000001", companyName: "Google", email: "google@gmail.com", telephone: "123456789", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ",displayDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '},
-    branches: ["IT", "Software Development"],
+    industries: ["IT", "Software Development"],
     localizations: ["Warsaw", "Krakow"],
     forms: ["B2B", "UoP"],
-    salaryStart: 5000,
-    salaryEnd: 10000,
-    contract_type: ["Full-time", "Part-time"],
-    working_time: "8h",
+    salaryFrom: 5000,
+    salaryTo: 10000,
+    contractType: ["Full-time", "Part-time"],
+    workingTime: "8h",
     level: ["Junior",'Mid'],
     requirements: ["Java", "Spring", "Hibernate", "SQL"],
-    extra_skills: ["Angular", "React", "Vue"],
+    extraSkills: ["Angular", "React", "Vue"],
     duties: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae ali",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl."
   }
