121ddb82a54ee6692791f9399c5a16740c6a21ff
Mateusz Pietrych <55856493+234mati@users.noreply.github.com>
2023-11-02 20:00:43 +0100

    MIWM-292: Refactor login panel and edit employee panel (#23)

--- src/app/worker/worker-account/worker-info-form/worker-info-form.component.spec.ts
+++ src/app/worker/worker-account/worker-info-form/worker-info-form.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkerInfoFormComponent } from './worker-info-form.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClientModule} from "@angular/common/http";
+import {ExpandedModuleFormComponent} from "../../../utilities/expanded-module-form/expanded-module-form.component";
+import {FormsModule} from "@angular/forms";
 
 describe('InfoFormComponent', () => {
   let component: WorkerInfoFormComponent;
@@ -8,7 +14,8 @@ describe('InfoFormComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [WorkerInfoFormComponent]
+      imports: [MatDialogModule,RouterTestingModule,HttpClientTestingModule,HttpClientModule, FormsModule],
+      declarations: [WorkerInfoFormComponent, ExpandedModuleFormComponent]
     });
     fixture = TestBed.createComponent(WorkerInfoFormComponent);
     component = fixture.componentInstance;
