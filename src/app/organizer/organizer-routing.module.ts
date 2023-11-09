8f069e6b80a8e0b91329c0529d92b96d81a515cb
234mati <mateusz.pietrych@gmail.com>
2023-11-09 18:26:35 +0100

    MIWM-317: Add organizer edit form

--- src/app/organizer/organizer-routing.module.ts
+++ src/app/organizer/organizer-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {OrganizerAccountComponent} from "./organizer-account/organizer-account.component";
+import {OrganizerInfoFormComponent} from "./organizer-account/organizer-info-form/organizer-info-form.component";
 
 const routes: Routes = [
   { path: 'organizer/account', component: OrganizerAccountComponent },
   { path: 'organizer',redirectTo: 'organizer/account', pathMatch: 'full' },
+  { path: 'organizer/edit-info', component: OrganizerInfoFormComponent }
 ];
 
 @NgModule({
