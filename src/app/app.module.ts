3337bf2b3d53165040d71a9d38d6af7572b25bfe
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-12 15:35:42 +0100

    MIWM-393: Add material design to login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppNavComponent } from './app-nav/app-nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from "@angular/material/form-field";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -24,6 +25,23 @@ import {UtilitiesModule} from "./utilities/utilities.module";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import { TestBed } from '@angular/core/testing';
 import {LoginService} from "./login/service/login.service";
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { FooterComponent } from './footer/footer.component';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -32,6 +50,7 @@ import {LoginService} from "./login/service/login.service";
     LoginComponent,
     RegisterComponent,
     AppComponent,
+    FooterComponent,
   ],
   imports: [
     HttpClientModule,
@@ -48,7 +67,24 @@ import {LoginService} from "./login/service/login.service";
     OrganizerModule,
     EmployerModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
 
   providers: [
