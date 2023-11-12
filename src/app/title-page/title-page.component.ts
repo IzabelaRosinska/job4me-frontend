99b13a4870491e3fbd845ab2707c215adcd5a121
234mati <mateusz.pietrych@gmail.com>
2023-11-13 00:01:45 +0100

    MIWM-382: Create title page

---
+++ src/app/title-page/title-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-page',
+  templateUrl: './title-page.component.html',
+  styleUrls: ['./title-page.component.scss']
+})
+export class TitlePageComponent {
+
+}
