11628c796fefe170e344936ae0497fa6aeb24887
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-12 23:17:07 +0100

    MIWM-393: Delete unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {EmployeeModule} from "./employee/employee.module";
 import {OrganizerModule} from "./organizer/organizer.module";
 import {EmployerModule} from "./employer/employer.module";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppNavComponent } from './app-nav/app-nav.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppNavComponent} from './app-nav/app-nav.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from "@angular/material/form-field";
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { LoginComponent } from './login/login.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {LoginComponent} from './login/login.component';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS,  HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {RequestInterceptor} from "./interceptors/request.interceptor";
-import { RegisterComponent } from './login/register/register.component';
-import {UtilitiesModule} from "./utilities/utilities.module";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import { TestBed } from '@angular/core/testing';
-import {LoginService} from "./login/service/login.service";
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
-import { FooterComponent } from './footer/footer.component';
+import {RegisterComponent} from './login/register/register.component';
+import {MdbCheckboxModule} from 'mdb-angular-ui-kit/checkbox';
+import {MdbDropdownModule} from 'mdb-angular-ui-kit/dropdown';
+import {MdbFormsModule} from 'mdb-angular-ui-kit/forms';
+import {MdbModalModule} from 'mdb-angular-ui-kit/modal';
+import {MdbPopoverModule} from 'mdb-angular-ui-kit/popover';
+import {MdbRadioModule} from 'mdb-angular-ui-kit/radio';
+import {MdbRangeModule} from 'mdb-angular-ui-kit/range';
+import {MdbValidationModule} from 'mdb-angular-ui-kit/validation';
+import {FooterComponent} from './footer/footer.component';
 import {MatInputModule} from '@angular/material/input';
 
 
@@ -68,20 +57,13 @@ import {MatInputModule} from '@angular/material/input';
     EmployerModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MdbAccordionModule,
-    MdbCarouselModule,
     MdbCheckboxModule,
-    MdbCollapseModule,
     MdbDropdownModule,
     MdbFormsModule,
     MdbModalModule,
     MdbPopoverModule,
     MdbRadioModule,
     MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
     MdbValidationModule,
     MatInputModule,
     MatFormFieldModule
@@ -96,4 +78,5 @@ import {MatInputModule} from '@angular/material/input';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
