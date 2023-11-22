e6d746bbf3edd0a69bc0c4121c6b4c4031bd1ad4
234mati <mateusz.pietrych@gmail.com>
2023-11-22 13:55:18 +0100

    MIWM-320: Change comments

--- src/app/organizer/jobfair/jobfair.component.ts
+++ src/app/organizer/jobfair/jobfair.component.ts
@@ -1,79 +1,46 @@
 import {Component, OnInit} from '@angular/core';
-import {
-    EmployerAccount,
-    ForListBackend,
-    ItemInsideList,
-    JobFair,
-    JobOffer,
-    Page,
-    PaginationUse,
-    ParticipationRequest
-} from "../../types";
-import {BehaviorSubject, catchError, Observable, of, startWith} from "rxjs";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {ForListBackend, ItemInsideList, JobFair, Page, PaginationUse,} from "../../types";
+import {Observable} from "rxjs";
+
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {JobfairService} from "../services/jobfair.service";
-import {PageEvent} from "@angular/material/paginator";
-import {map} from "rxjs/operators";
 import {PaginationService} from "../../utilities/service/pagination.service";
 
 @Component({
-  selector: 'app-jobfair',
-  templateUrl: './jobfair.component.html',
-  styleUrls: ['./jobfair.component.scss']
+    selector: 'app-jobfair',
+    templateUrl: './jobfair.component.html',
+    styleUrls: ['./jobfair.component.scss']
 })
-export class JobfairComponent implements OnInit{
+export class JobfairComponent implements OnInit {
 
-  jobFairsState$!: Observable<{ appState: string, appData?: Page<ForListBackend>, error?: HttpErrorResponse }>;
-  responseSubject = new BehaviorSubject<Page<ForListBackend>>({} as Page<ForListBackend>);
-  private currentPageSubject = new BehaviorSubject<number>(0);
-  currentPage$ = this.currentPageSubject.asObservable();
+    pageSize: number = 5;
+    pageIndex: number = 0;
+    length: number = 20;
+    employersAsList: ItemInsideList[] = [];
+    companyPhoto = '../../assets/company.png';
+    filters: [string, string][] = [["Minimalne wynagrodzenie", ""], ["Branża", "/industries"]];
 
+    loadingSite: boolean = true;
+    currentTabId = "jobFairs";
+    paginationUseList: PaginationUse<ForListBackend>[] = [];
+    isOwner: boolean = false;
 
+    getPaginationService() {
+        return this.servicePagination;
+    }
 
-  pageEvent?: PageEvent;
-  pageSize: number = 5;
-  pageIndex: number = 0;
-  length: number = 20;
-  employersAsList: ItemInsideList[] = [];
-  companyPhoto = '../../assets/company.png';
-  filters: [string, string][] = [["Minimalne wynagrodzenie", ""], ["Branża", "/industries"]];
-
-  tabs: [key: string, value: boolean][] = [
-    ["tab1", true],
-    ["tab2", false]
-  ]
-
-
-  loadingSite: boolean = true;
-  currentTabId = "jobFairs";
-  paginationUseList: PaginationUse<ForListBackend>[] = [];
-  isOwner: boolean = false;
-
-  getPaginationService(){
-    return this.servicePagination;
-  }
-
-  constructor(public route: ActivatedRoute,
+    constructor(public route: ActivatedRoute,
                 private serviceJobFair: JobfairService,
                 private servicePagination: PaginationService) {
-      const role = localStorage.getItem('role');
+        const role = localStorage.getItem('role');
 
-  }
-
-  getDisplayList(tabId: string): ItemInsideList[]{
-    const paginationUse = this.servicePagination.getPaginationUseById(tabId, this.paginationUseList);
-    if(paginationUse){
-      return paginationUse.list;
     }
-    return [];
-  }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
         this.route.paramMap.subscribe((params: ParamMap) => {
             const jobfairId = Number(params.get('jobfair-id'));
-            if(jobfairId){
+            if (jobfairId) {
                 this.paginationUseList = [
                     {
                         id: "employers",
@@ -82,7 +49,7 @@ export class JobfairComponent implements OnInit{
                         pageIndex: 0,
                         length: 20,
                         state: new Observable<Page<ForListBackend>>(),
-                        route:  "/job-fairs/"+jobfairId+"/employers",
+                        route: "/job-fairs/" + jobfairId + "/employers",
                         list: [],
                         loading: true,
                         ListButtonsOptions: {
@@ -100,7 +67,7 @@ export class JobfairComponent implements OnInit{
                         pageIndex: 0,
                         length: 20,
                         state: new Observable<Page<ForListBackend>>(),
-                        route: "/job-fairs/"+jobfairId+"/job-offers/list-display",
+                        route: "/job-fairs/" + jobfairId + "/job-offers/list-display",
                         list: [],
                         loading: true,
                         ListButtonsOptions: {
@@ -113,7 +80,6 @@ export class JobfairComponent implements OnInit{
                     }
                 ]
 
-
                 this.serviceJobFair.getJobFairById(jobfairId).subscribe((response: JobFair) => {
                     this.jobFair.id = response.id;
                     this.jobFair.name = response.name;
@@ -125,7 +91,7 @@ export class JobfairComponent implements OnInit{
                     this.jobFair.displayDescription = response.displayDescription;
                     this.loading = false;
 
-                    console.log( "dateStart: " + this.jobFair.dateStart);
+                    console.log("dateStart: " + this.jobFair.dateStart);
                     console.log("dateEnd: " + this.jobFair.dateEnd);
                 });
 
@@ -133,140 +99,55 @@ export class JobfairComponent implements OnInit{
                     this.servicePagination.changePaginationState(paginationUse, paginationUse.ListButtonsOptions);
                 });
 
-                // this.jobFairsState$ = this.serviceJobFair.participationEmployers$(jobfairId).pipe(
-                //     map((response) => {
-                //         console.log(response);
-                //         this.responseSubject.next(response);
-                //         this.currentPageSubject.next(response.number);
-                //
-                //         response.content.forEach(
-                //             (offer) => {
-                //                 this.addEmployerForList(offer);
-                //             }
-                //         );
-                //
-                //         this.loadingEmployers = false;
-                //
-                //         return ({appState: 'APP_LOADED', appData: response});
-                //     }),
-                //     startWith({appState: 'APP_LOADED'}),
-                //     catchError((error: HttpErrorResponse) => {
-                //             return of({appState: 'APP_ERROR', error})
-                //         }
-                //     )
-                // )
-            }
-            else{
+            } else {
                 this.loading = false;
             }
         });
 
-      this.paginationUseList.forEach((paginationUse: PaginationUse<ForListBackend>) => {
-          paginationUse.state.subscribe((response) => {
-              paginationUse.length = response ? response.totalElements : 0;
-              paginationUse.pageSize = response ? response.size : 0;
-              paginationUse.pageIndex = response ? response.number : 0;
-          });
-      });
-
-        // this.jobFairsState$.subscribe((response) => {
-        //     this.length = response.appData ? response.appData.totalElements : 0;
-        //     this.pageSize = response.appData ? response.appData.size : 0;
-        //     this.pageIndex = response.appData ? response.appData.number : 0;
-        // });
+        this.paginationUseList.forEach((paginationUse: PaginationUse<ForListBackend>) => {
+            paginationUse.state.subscribe((response) => {
+                paginationUse.length = response ? response.totalElements : 0;
+                paginationUse.pageSize = response ? response.size : 0;
+                paginationUse.pageIndex = response ? response.number : 0;
+            });
+        });
     }
 
+    loading: boolean = true;
 
+    jobFair: JobFair = {
+        id: 0,
+        name: "",
+        organizerId: 0,
+        dateStart: "",
+        dateEnd: "",
+        address: "",
+        description: "",
+        displayDescription: ""
+    }
 
-    changeTab(tabId: string) {
-        this.currentTabId = tabId;
-        this.paginationUseList.map((elem) => {
-            if (elem.id === tabId) {
-                elem.active = true;
-            } else {
-                elem.active = false;
+    addEmployerForList(employer: ForListBackend): void {
+        const role = localStorage.getItem('role');
+        let employerAsItemInsideList: ItemInsideList = {
+            route: "/employer/" + employer.id + "/account",
+            image: employer.photo ? employer.photo : this.companyPhoto,
+            name: employer.name,
+            id: employer.id ? employer.id : 0,
+            displayDescription: `${employer.displayDescription}`,
+            ListButtonsOptions: {
+                useGettingInside: true,
+                useApprove: false,
+                useSaved: false,
+                isSaved: false,
+                useDelete: false
             }
-        })
+
+        }
+        this.employersAsList.push(employerAsItemInsideList);
     }
 
-  foundValueByNameInTab(tab: string): boolean{
-    return this.tabs.filter((elem) => elem[0] === tab)[0][1];
-  }
-
-  loading: boolean = true;
-  loadingEmployers = true;
-
-  jobFair: JobFair = {
-    id: 0,
-    name: "",
-    organizerId: 0,
-    dateStart: "",
-    dateEnd: "",
-    address: "",
-    description: "",
-    displayDescription: ""
-  }
-
-  handlePageEvent(e: PageEvent) {
-    this.pageEvent = e;
-    this.length = e.length;
-    this.pageSize = e.pageSize;
-    this.pageIndex = e.pageIndex;
-    this.gotToPage(this.jobFair.id,'', e.pageIndex);
-  }
-
-
-
-  addEmployerForList(employer: ForListBackend): void {
-    const role = localStorage.getItem('role');
-    let employerAsItemInsideList: ItemInsideList = {
-      route: "/employer/"+employer.id+"/account",
-      image: employer.photo ? employer.photo : this.companyPhoto,
-      name: employer.name,
-      id: employer.id ? employer.id : 0,
-      displayDescription: `${employer.displayDescription}`,
-      ListButtonsOptions: {
-          useGettingInside: true,
-          useApprove: false,
-          useSaved: false,
-          isSaved: false,
-          useDelete: false
-      }
-
+    convertDate(date: string): string {
+        return date.substring(0, 10) + " " + date.substring(11);
     }
-    console.log("Route: " + employerAsItemInsideList.route);
-    this.employersAsList.push(employerAsItemInsideList);
-  }
-
-  gotToPage(jobfairId: number ,name?: string, pageNumber: number = 0): void {
-    this.employersAsList = [];
-    this.loadingEmployers = true;
-    this.jobFairsState$ = this.serviceJobFair.participationEmployers$(jobfairId, pageNumber, this.pageSize).pipe(
-        map((response: Page<ForListBackend>) => {
-
-          this.responseSubject.next(response);
-          this.currentPageSubject.next(response.number);
-
-          response.content.forEach(
-              (employerForList) => {
-                this.addEmployerForList(employerForList);
-              }
-          );
-          this.loadingEmployers = false;
-          console.log(response);
-          return ({appState: 'APP_LOADED', appData: response});
-        }),
-        startWith({appState: 'APP_LOADED', appData: this.responseSubject.value}),
-        catchError((error: HttpErrorResponse) => {
-              return of({appState: 'APP_ERROR', error})
-            }
-        )
-    )
-
-  }
-
-  convertDate(date: string): string{
-    return date.substring(0,10)+" "+date.substring(11);
-  }
 
 }
--- src/app/utilities/service/pagination.service.ts
+++ src/app/utilities/service/pagination.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ForListBackend, ItemInsideList, ListButtonsOptions, Page, PaginationUse} from "../../types";
 import {map, shareReplay} from "rxjs/operators";
 import {PageEvent} from "@angular/material/paginator";
@@ -7,132 +7,121 @@ import {ROUTES} from "../../../environments/environments";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PaginationService {
 
-  constructor(private http: HttpClient) {}
-
-  basePhoto = '../../assets/company.png';
-  currentTabId: string = "";
-
-  getPaginationUseById(id: string, paginationList: PaginationUse<ForListBackend>[]): PaginationUse<ForListBackend> | null {
-    const elem = paginationList.find(item => item.id === id);
-    if (elem == undefined) {
-      return null;
-    }
-    return elem;
-  }
-
-  getDisplayListById(tabId: string, paginationUseList: PaginationUse<ForListBackend>[]): ItemInsideList[]{
-    const paginationUse = this.getPaginationUseById(tabId, paginationUseList);
-    if(paginationUse){
-      return paginationUse.list;
-    }
-    return [];
-  }
-
-  isActiveById(id: string, paginationUseList: PaginationUse<ForListBackend>[]): boolean{
-    const paginationUse = this.getPaginationUseById(id, paginationUseList);
-    if(paginationUse){
-      return paginationUse?.active;
-    }
-    return false;
-  }
-
-
-  changePaginationState(paginationUse: PaginationUse<ForListBackend>, listButtonsOptions: ListButtonsOptions | undefined): void {
-    paginationUse.list = [];
-    paginationUse.loading = true;
-    paginationUse.state = this.paginateDataWithParams$(
-        paginationUse.route, paginationUse.pageIndex, paginationUse.pageSize, paginationUse.params ? paginationUse.params : '').pipe(
-        map((response: Page<ForListBackend>) => {
-              response.content.forEach(
-                  (elem) => {
-                    paginationUse.list.push(this.addElementToDisplayList(elem, paginationUse.route, listButtonsOptions));
-                  }
-              );
-              paginationUse.loading = false;
-              console.log(paginationUse.id + " " + paginationUse.loading)
-              return response;
-            }
-        ));
-  }
-
-  addElementToDisplayList(elem: ForListBackend, route: string, listButtonsOptions: ListButtonsOptions | undefined): ItemInsideList {
-
-    let insideList: ItemInsideList = {
-      route: route + elem.id,
-      image: elem.photo ? elem.photo : this.basePhoto,
-      name: elem.name,
-      id: elem.id,
-      displayDescription: elem.displayDescription,
-      ListButtonsOptions: {
-        useSaved: listButtonsOptions? listButtonsOptions.useSaved : false,
-        isSaved: listButtonsOptions? listButtonsOptions.isSaved : false,
-        useDelete: listButtonsOptions? listButtonsOptions.useDelete : false,
-        useApprove: listButtonsOptions? listButtonsOptions.useApprove : false,
-        useGettingInside: listButtonsOptions? listButtonsOptions.useGettingInside : false
-      }
-    }
-    return insideList;
-  }
-
-  handlePageEvent(pe: PageEvent, tabId: string, paginationUseList: PaginationUse<ForListBackend>[]) {
-    const paginationUse = this.getPaginationUseById(tabId, paginationUseList);
-    if(paginationUse){
-      paginationUse.length = pe.length;
-      paginationUse.pageSize = pe.pageSize;
-      paginationUse.pageIndex = pe.pageIndex;
-      this.gotToPage(paginationUseList,paginationUse.id);
-      paginationUse.state.subscribe((response) => {
-        paginationUse.length = response ? response.totalElements : 0;
-      });
+    constructor(private http: HttpClient) {
     }
 
-  }
+    basePhoto = '../../assets/company.png';
+    currentTabId: string = "";
 
-  gotToPage(paginationUseList: PaginationUse<ForListBackend>[], paginationUseId?: string): void {
-    const elem = this.getPaginationUseById(paginationUseId ? paginationUseId : this.currentTabId, paginationUseList);
-    if (elem) {
-      this.changePaginationState(elem, elem.ListButtonsOptions);
-    }
-  }
-
-  changeTab(tabId: string, paginationUseList: PaginationUse<ForListBackend>[]) {
-    this.currentTabId = tabId;
-    paginationUseList.map((elem) => {
-      if (elem.id === tabId) {
-        elem.active = true;
-      } else {
-        elem.active = false;
-      }
-    })
-  }
-
-  // deleteRequest(id: number): void {
-  //   this.serviceOrganizer.deleteEmployerParticipation(id).subscribe((response) => {
-  //     this.updateCurrentTabIdPagination();
-  //   });
-  // }
-  //
-  // acceptRequest(id: number): void {
-  //   this.serviceOrganizer.acceptEmployerParticipation(id).subscribe((response) => {
-  //     this.updateCurrentTabIdPagination();
-  //   });
-  // }
-
-  updateCurrentTabIdPagination(paginationUseList: PaginationUse<ForListBackend>[]): void {
-      const elem = this.getPaginationUseById(this.currentTabId,paginationUseList);
-        if(elem){
-          elem.length = elem.length - 1;
-          this.gotToPage(paginationUseList);
-          elem.state.subscribe((response) => {});
+    getPaginationUseById(id: string, paginationList: PaginationUse<ForListBackend>[]): PaginationUse<ForListBackend> | null {
+        const elem = paginationList.find(item => item.id === id);
+        if (elem == undefined) {
+            return null;
         }
-      }
+        return elem;
+    }
 
-  paginateDataWithParams$ = (route: string, page: number = 0, size: number = 5, params?: string): Observable<Page<ForListBackend>> =>
-      this.http.get<Page<ForListBackend>>(`${ROUTES.BACKEND_ROUTE}${route}?&page=${page}&size=${size}&order=1${params?params:''}`).pipe(shareReplay(1));
+    getDisplayListById(tabId: string, paginationUseList: PaginationUse<ForListBackend>[]): ItemInsideList[] {
+        const paginationUse = this.getPaginationUseById(tabId, paginationUseList);
+        if (paginationUse) {
+            return paginationUse.list;
+        }
+        return [];
+    }
+
+    isActiveById(id: string, paginationUseList: PaginationUse<ForListBackend>[]): boolean {
+        const paginationUse = this.getPaginationUseById(id, paginationUseList);
+        if (paginationUse) {
+            return paginationUse?.active;
+        }
+        return false;
+    }
+
+
+    changePaginationState(paginationUse: PaginationUse<ForListBackend>, listButtonsOptions: ListButtonsOptions | undefined): void {
+        paginationUse.list = [];
+        paginationUse.loading = true;
+        paginationUse.state = this.paginateDataWithParams$(
+            paginationUse.route, paginationUse.pageIndex, paginationUse.pageSize, paginationUse.params ? paginationUse.params : '').pipe(
+            map((response: Page<ForListBackend>) => {
+                    response.content.forEach(
+                        (elem) => {
+                            paginationUse.list.push(this.addElementToDisplayList(elem, paginationUse.route, listButtonsOptions));
+                        }
+                    );
+                    paginationUse.loading = false;
+                    return response;
+                }
+            ));
+    }
+
+    addElementToDisplayList(elem: ForListBackend, route: string, listButtonsOptions: ListButtonsOptions | undefined): ItemInsideList {
+
+        let insideList: ItemInsideList = {
+            route: route + elem.id,
+            image: elem.photo ? elem.photo : this.basePhoto,
+            name: elem.name,
+            id: elem.id,
+            displayDescription: elem.displayDescription,
+            ListButtonsOptions: {
+                useSaved: listButtonsOptions ? listButtonsOptions.useSaved : false,
+                isSaved: listButtonsOptions ? listButtonsOptions.isSaved : false,
+                useDelete: listButtonsOptions ? listButtonsOptions.useDelete : false,
+                useApprove: listButtonsOptions ? listButtonsOptions.useApprove : false,
+                useGettingInside: listButtonsOptions ? listButtonsOptions.useGettingInside : false
+            }
+        }
+        return insideList;
+    }
+
+    handlePageEvent(pe: PageEvent, tabId: string, paginationUseList: PaginationUse<ForListBackend>[]) {
+        const paginationUse = this.getPaginationUseById(tabId, paginationUseList);
+        if (paginationUse) {
+            paginationUse.length = pe.length;
+            paginationUse.pageSize = pe.pageSize;
+            paginationUse.pageIndex = pe.pageIndex;
+            this.gotToPage(paginationUseList, paginationUse.id);
+            paginationUse.state.subscribe((response) => {
+                paginationUse.length = response ? response.totalElements : 0;
+            });
+        }
+
+    }
+
+    gotToPage(paginationUseList: PaginationUse<ForListBackend>[], paginationUseId?: string): void {
+        const elem = this.getPaginationUseById(paginationUseId ? paginationUseId : this.currentTabId, paginationUseList);
+        if (elem) {
+            this.changePaginationState(elem, elem.ListButtonsOptions);
+        }
+    }
+
+    changeTab(tabId: string, paginationUseList: PaginationUse<ForListBackend>[]) {
+        this.currentTabId = tabId;
+        paginationUseList.map((elem) => {
+            if (elem.id === tabId) {
+                elem.active = true;
+            } else {
+                elem.active = false;
+            }
+        })
+    }
+
+    updateCurrentTabIdPagination(paginationUseList: PaginationUse<ForListBackend>[]): void {
+        const elem = this.getPaginationUseById(this.currentTabId, paginationUseList);
+        if (elem) {
+            elem.length = elem.length - 1;
+            this.gotToPage(paginationUseList);
+            elem.state.subscribe((response) => {
+            });
+        }
+    }
+
+    paginateDataWithParams$ = (route: string, page: number = 0, size: number = 5, params?: string): Observable<Page<ForListBackend>> =>
+        this.http.get<Page<ForListBackend>>(`${ROUTES.BACKEND_ROUTE}${route}?&page=${page}&size=${size}&order=1${params ? params : ''}`).pipe(shareReplay(1));
 
 
 }
