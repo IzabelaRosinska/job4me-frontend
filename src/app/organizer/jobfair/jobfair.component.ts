d5b0e918e32028faeceec0c8a9b30851c6db5a1f
234mati <mateusz.pietrych@gmail.com>
2023-11-24 04:25:40 +0100

    MIWM-379: Delete comments

--- src/app/organizer/jobfair/jobfair.component.ts
+++ src/app/organizer/jobfair/jobfair.component.ts
@@ -15,159 +15,157 @@ import {JobfairService} from "../services/jobfair.service";
 import {PaginationService} from "../../utilities/service/pagination.service";
 
 @Component({
-    selector: 'app-jobfair',
-    templateUrl: './jobfair.component.html',
-    styleUrls: ['./jobfair.component.scss']
+  selector: 'app-jobfair',
+  templateUrl: './jobfair.component.html',
+  styleUrls: ['./jobfair.component.scss']
 })
 export class JobfairComponent implements OnInit {
 
-    pageSize: number = 5;
-    pageIndex: number = 0;
-    length: number = 20;
-    employersAsList: ItemInsideList[] = [];
-    companyPhoto = '../../assets/company.png';
-    // filters: [string, string][] = [["Minimalne wynagrodzenie", ""], ["Branża", "/industries"]];
-    filters: JobOfferFilterDto = {
-        contractTypeNames: [
-            "umowa o pracę"
+  pageSize: number = 5;
+  length: number = 20;
+  employersAsList: ItemInsideList[] = [];
+  companyPhoto = '../../assets/company.png';
+  filters: JobOfferFilterDto = {
+    contractTypeNames: [
+      "umowa o pracę"
+    ]
+  };
+
+  loadingSite: boolean = true;
+  currentTabId = "jobFairs";
+  paginationUseList: PaginationUse<ForListBackend>[] = [];
+  isOwner: boolean = false;
+
+  getPaginationService() {
+    return this.servicePagination;
+  }
+
+  constructor(public route: ActivatedRoute,
+              private serviceJobFair: JobfairService,
+              private servicePagination: PaginationService) {
+    const role = localStorage.getItem('role');
+
+  }
+
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const role = localStorage.getItem('role');
+      const jobfairId = Number(params.get('jobfair-id'));
+      if (jobfairId) {
+        this.paginationUseList = [
+          {
+            id: "employers",
+            active: true,
+            pageSize: 5,
+            pageIndex: 0,
+            length: 20,
+            state: new Observable<Page<ForListBackend>>(),
+            route: "/job-fairs/" + jobfairId + "/employers",
+            routeToElement: "/" + role + "/employer/",
+            list: [],
+            loading: true,
+            ListButtonsOptions: {
+              useGettingInside: true,
+              useDelete: this.isOwner,
+              useSaved: false,
+              isSaved: false,
+              useApprove: false
+            },
+            ifGet: true,
+            filters: null
+          },
+          {
+            id: "job-offers",
+            active: false,
+            pageSize: 5,
+            pageIndex: 0,
+            length: 20,
+            state: new Observable<Page<ForListBackend>>(),
+            route: "/job-fairs/" + jobfairId + "/job-offers/list-display",
+            routeToElement: "/employer/job-offer/",
+            list: [],
+            loading: true,
+            ListButtonsOptions: {
+              useGettingInside: true,
+              useDelete: false,
+              useSaved: false,
+              isSaved: false,
+              useApprove: false
+            },
+            ifGet: false,
+            filters: {}
+          }
         ]
-    };
 
-    loadingSite: boolean = true;
-    currentTabId = "jobFairs";
-    paginationUseList: PaginationUse<ForListBackend>[] = [];
-    isOwner: boolean = false;
+        this.serviceJobFair.getJobFairById(jobfairId).subscribe((response: JobFair) => {
+          this.jobFair.id = response.id;
+          this.jobFair.name = response.name;
+          this.jobFair.organizerId = response.organizerId;
+          this.jobFair.dateStart = this.convertDate(response.dateStart);
+          this.jobFair.dateEnd = this.convertDate(response.dateEnd);
+          this.jobFair.address = response.address;
+          this.jobFair.description = response.description;
+          this.jobFair.displayDescription = response.displayDescription;
+          this.loading = false;
 
-    getPaginationService() {
-        return this.servicePagination;
-    }
-
-    constructor(public route: ActivatedRoute,
-                private serviceJobFair: JobfairService,
-                private servicePagination: PaginationService) {
-        const role = localStorage.getItem('role');
-
-    }
-
-    ngOnInit(): void {
-
-        this.route.paramMap.subscribe((params: ParamMap) => {
-            const role = localStorage.getItem('role');
-            const jobfairId = Number(params.get('jobfair-id'));
-            if (jobfairId) {
-                this.paginationUseList = [
-                    {
-                        id: "employers",
-                        active: true,
-                        pageSize: 5,
-                        pageIndex: 0,
-                        length: 20,
-                        state: new Observable<Page<ForListBackend>>(),
-                        route: "/job-fairs/" + jobfairId + "/employers",
-                        routeToElement: "/"+role+"/employer/",
-                        list: [],
-                        loading: true,
-                        ListButtonsOptions: {
-                            useGettingInside: true,
-                            useDelete: this.isOwner,
-                            useSaved: false,
-                            isSaved: false,
-                            useApprove: false
-                        },
-                        ifGet: true,
-                        filters: null
-                    },
-                    {
-                        id: "job-offers",
-                        active: false,
-                        pageSize: 5,
-                        pageIndex: 0,
-                        length: 20,
-                        state: new Observable<Page<ForListBackend>>(),
-                        route: "/job-fairs/" + jobfairId + "/job-offers/list-display",
-                        routeToElement: "/employer/job-offer/",
-                        list: [],
-                        loading: true,
-                        ListButtonsOptions: {
-                            useGettingInside: true,
-                            useDelete: false,
-                            useSaved: false,
-                            isSaved: false,
-                            useApprove: false
-                        },
-                        ifGet: false,
-                        filters: {}
-                    }
-                ]
-
-                this.serviceJobFair.getJobFairById(jobfairId).subscribe((response: JobFair) => {
-                    this.jobFair.id = response.id;
-                    this.jobFair.name = response.name;
-                    this.jobFair.organizerId = response.organizerId;
-                    this.jobFair.dateStart = this.convertDate(response.dateStart);
-                    this.jobFair.dateEnd = this.convertDate(response.dateEnd);
-                    this.jobFair.address = response.address;
-                    this.jobFair.description = response.description;
-                    this.jobFair.displayDescription = response.displayDescription;
-                    this.loading = false;
-
-                });
-
-                this.paginationUseList.forEach((paginationUse: PaginationUse<ForListBackend>) => {
-                    this.servicePagination.changePaginationState(paginationUse, paginationUse.ListButtonsOptions);
-                });
-
-            } else {
-                this.loading = false;
-            }
         });
 
         this.paginationUseList.forEach((paginationUse: PaginationUse<ForListBackend>) => {
-            paginationUse.state.subscribe((response) => {
-                paginationUse.length = response ? response.totalElements : 0;
-                paginationUse.pageSize = response ? response.size : 0;
-                paginationUse.pageIndex = response ? response.number : 0;
-            });
-
+          this.servicePagination.changePaginationState(paginationUse, paginationUse.ListButtonsOptions);
         });
+
+      } else {
+        this.loading = false;
+      }
+    });
+
+    this.paginationUseList.forEach((paginationUse: PaginationUse<ForListBackend>) => {
+      paginationUse.state.subscribe((response) => {
+        paginationUse.length = response ? response.totalElements : 0;
+        paginationUse.pageSize = response ? response.size : 0;
+        paginationUse.pageIndex = response ? response.number : 0;
+      });
+
+    });
+  }
+
+  loading: boolean = true;
+
+  jobFair: JobFair = {
+    id: 0,
+    name: "",
+    organizerId: 0,
+    dateStart: "",
+    dateEnd: "",
+    address: "",
+    description: "",
+    displayDescription: ""
+  }
+
+  addEmployerForList(employer: ForListBackend): void {
+    const role = localStorage.getItem('role');
+    let employerAsItemInsideList: ItemInsideList = {
+      route: "/employer/" + employer.id + "/account",
+      image: employer.photo ? employer.photo : this.companyPhoto,
+      name: employer.name,
+      id: employer.id ? employer.id : 0,
+      displayDescription: `${employer.displayDescription}`,
+      ListButtonsOptions: {
+        useGettingInside: true,
+        useApprove: false,
+        useSaved: false,
+        isSaved: false,
+        useDelete: false
+      }
+
     }
+    this.employersAsList.push(employerAsItemInsideList);
+  }
 
-    loading: boolean = true;
-
-    jobFair: JobFair = {
-        id: 0,
-        name: "",
-        organizerId: 0,
-        dateStart: "",
-        dateEnd: "",
-        address: "",
-        description: "",
-        displayDescription: ""
-    }
-
-    addEmployerForList(employer: ForListBackend): void {
-        const role = localStorage.getItem('role');
-        let employerAsItemInsideList: ItemInsideList = {
-            route: "/employer/" + employer.id + "/account",
-            image: employer.photo ? employer.photo : this.companyPhoto,
-            name: employer.name,
-            id: employer.id ? employer.id : 0,
-            displayDescription: `${employer.displayDescription}`,
-            ListButtonsOptions: {
-                useGettingInside: true,
-                useApprove: false,
-                useSaved: false,
-                isSaved: false,
-                useDelete: false
-            }
-
-        }
-        this.employersAsList.push(employerAsItemInsideList);
-    }
-
-    convertDate(date: string): string {
-        return date.substring(0, 10) + " " + date.substring(11);
-    }
+  convertDate(date: string): string {
+    return date.substring(0, 10) + " " + date.substring(11);
+  }
 
   protected readonly FiliterType = FiliterType;
 
--- src/app/utilities/filter-sort-panel/filter-sort-panel.component.ts
+++ src/app/utilities/filter-sort-panel/filter-sort-panel.component.ts
@@ -1,8 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {UtilitiesService} from "../service/utilities.service";
-import {BehaviorSubject, Observable} from "rxjs";
-import {FiliterType, JobOffer, Page} from "../../types";
-import {HttpErrorResponse} from "@angular/common/http";
+import {FiliterType} from "../../types";
 import {VariablesService} from "../service/variables.service";
 
 
@@ -21,45 +18,30 @@ export class FilterSortPanelComponent implements OnInit {
   filterOptionSelected: string[] = [];
   start: boolean = false;
 
-  @Output() filterOptionSelectedOutput: EventEmitter<[FiliterType,string[]][]> = new EventEmitter<[FiliterType,string[]][]>();
-  filterOptionSelectedOutputPrepare: [FiliterType,string[]][] = [];
+  @Output() filterOptionSelectedOutput: EventEmitter<[FiliterType, string[]][]> = new EventEmitter<[FiliterType, string[]][]>();
+  filterOptionSelectedOutputPrepare: [FiliterType, string[]][] = [];
 
   @Output() sortOptionSelectedOutput: EventEmitter<number> = new EventEmitter<number>();
 
-  state$!: Observable<{ appState: string, appData?: Page<string[]>, error?: HttpErrorResponse }>;
-  responseSubject = new BehaviorSubject<Page<string[]>>({} as Page<string[]>);
-  private currentPageSubject = new BehaviorSubject<number>(0);
 
-  constructor(private utilitiesService: UtilitiesService,
-              private variablesService: VariablesService) {
+  constructor(private variablesService: VariablesService) {
   }
 
   ngOnInit(): void {
     for (let i = 0; i < this.filter.length; i++) {
       const filterName = this.filter[i];
       console.log(filterName + " " + this.variablesService.dictionaryIfLoaded[filterName]);
-      if(this.variablesService.dictionaryIfLoaded[filterName]){
-        console.log(this.variablesService.dictionaryOfLoadedData[filterName] + " " + filterName+ " " + this.variablesService.dictionaryOfLoadedData["industryNames"]);
+      if (this.variablesService.dictionaryIfLoaded[filterName]) {
+        console.log(this.variablesService.dictionaryOfLoadedData[filterName] + " " + filterName + " " + this.variablesService.dictionaryOfLoadedData["industryNames"]);
         const pair: [FiliterType, string[]] = [filterName, this.variablesService.dictionaryOfLoadedData[filterName]];
         this.filterOptions.push(pair);
         this.start = true;
-      }else{
-        const pair: [FiliterType, string[]] = [filterName,[]];
+      } else {
+        const pair: [FiliterType, string[]] = [filterName, []];
         this.filterOptions.push(pair);
         this.start = true;
       }
-      // if (this.filter[i][1] !== "") {
-      //   this.utilitiesService.getFilterOptions(this.filter[i][1]).subscribe((options) => {
-      //     const pair: [string, string[]] = [this.filter[i][0], options.content.map((elem) => elem.name) as string[]];
-      //     this.filterOptions.push(pair);
-      //     this.start = true;
-      //   });
-      // } else {
-      //   const pair: [string, string[]] = [this.filter[i][0], []];
-      //   this.filterOptions.push(pair);
-      //   this.start = true;
-      // }
-      this.filterOptionSelectedOutputPrepare.push([this.filter[i],[]]);
+      this.filterOptionSelectedOutputPrepare.push([this.filter[i], []]);
     }
     this.sortOptions = this.variablesService.getSortingOffersOptionsStrings();
   }
@@ -75,7 +57,7 @@ export class FilterSortPanelComponent implements OnInit {
 
   filterOptionClicked(title: FiliterType, options: string[]) {
     const index = this.findfilterOptionSelectedOutputPrepare(title);
-    if(index != -1) {
+    if (index != -1) {
       this.filterOptionSelectedOutputPrepare[index][1] = options;
     }
     this.filterOptionSelectedOutput.emit(this.filterOptionSelectedOutputPrepare);
@@ -84,10 +66,5 @@ export class FilterSortPanelComponent implements OnInit {
   sortOptionClicked(option: string) {
     this.sortOptionSelectedOutput.emit(this.variablesService.sortOffersOptions[option]);
   }
-
-  print(text: string) {
-    console.log(text)
-  }
-
 
 }
