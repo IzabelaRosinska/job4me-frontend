c93ec8db3d072dcecf50c5bb617d83eeae1bcbc4
234mati <mateusz.pietrych@gmail.com>
2023-12-15 06:52:03 +0100

    MIWM: Update backend route

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const ROUTES = {
-  BACKEND_ROUTE: 'http://localhost:4200/api',
+  BACKEND_ROUTE: 'https://job4me.azurewebsites.net',
   BASE_ROUTE: 'https://mango-moss-0c13e2b03.3.azurestaticapps.net',
 };
